905ae14c489233c1b2b3748d9a615c3f9e19320d
csslick <digitone@naver.com>
2021-04-03 23:43:00 +0900

    111

---
+++ src/components/Review.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import people from '../data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa';
+
+export default function Review() {
+  const [index, setIndex] = useState(0);
+  const {name, job, image, text} = people[index];
+  console.log(people)
+
+  function prevIndex() {
+    if(index == 0) {
+      setIndex(people.length - 1);
+      return;
+    }  
+    setIndex(index-1)
+  }
+
+  function nextIndex() {
+    if(index == people.length-1) {
+      setIndex(0);
+      return
+    }  
+    setIndex(index+1)
+  }
+
+  function randomIndex() {
+    const r = Math.floor(Math.random()*(people.length));
+    console.log(r);
+    if(r === index) {
+      nextIndex();
+    } else {
+      setIndex(r);
+    }
+  }
+
+  return (
+    <article className='card'>
+      <figure>
+        <img src={image} alt={name} />
+      </figure>
+      <h4 className="name">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button 
+          className="prev-btn"
+          onClick={prevIndex}
+        >
+          <FaChevronLeft />
+        </button>
+        <button 
+          className="next-btn"
+          onClick={nextIndex}
+        >
+          <FaChevronRight />
+        </button>
+        <button className="random-btn" onClick={randomIndex}>누가 나올까요?</button>
+      </div>
+    </article>
+  )
+}
+
